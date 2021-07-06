9c9b770d2ebe8959d96931f14a3353eaf0305933
danterusdev <danterusdev@gmail.com>
2021-07-06 16:50:04 -0700

    added transformer example for 1.8.9

--- impl/client/impl/1.8.9/src/main/java/io/github/glassmc/example/client/v1_8_9/ExampleClientInitializeListener.java
+++ impl/client/impl/1.8.9/src/main/java/io/github/glassmc/example/client/v1_8_9/ExampleClientInitializeListener.java
@@ -3,12 +3,16 @@ package io.github.glassmc.example.client.v1_8_9;
 import io.github.glassmc.example.client.IExampleInterface;
 import io.github.glassmc.loader.GlassLoader;
 import io.github.glassmc.loader.Listener;
+import io.github.glassmc.loader.util.Identifier;
 
 public class ExampleClientInitializeListener implements Listener {
 
     @Override
     public void run() {
         GlassLoader.getInstance().registerInterface(IExampleInterface.class, new ExampleInterfaceImpl());
+
+        GlassLoader.getInstance().registerTransformer(new MinecraftClientTransformer());
+        GlassLoader.getInstance().registerReloadClass(Identifier.parse("net/minecraft/client/MinecraftClient").getClassName());
     }
 
 }
